70431284fc900743fbc00f0067dd16838b21d60e
Marisa DeMeglio <marisa.demeglio@gmail.com>
2020-09-17 19:04:17 -0700

    Accepts argument of .env file on cmd line
    Also separated scripts in package.json: "makeCache" and "startApi"

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const express = require('express')
 const path = require('path');
+const envFile = process.argv.length > 2 ? 
+    process.argv[2] : ".env";
+const envpath = path.resolve(__dirname, envFile);
+require('dotenv').config({path: envpath});
+
+const express = require('express')
 const { postgraphile } = require("postgraphile");
 const { options } = require('./postgraphileOptions');
 const schemas = ['epubtest'];
 
-const app = express()
-
-const envpath = path.join(__dirname, '/.env');
-require('dotenv').config({path: envpath});
-
+const app = express();
 const DBURL = `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.DATABASE_HOST}:5432/${process.env.POSTGRES_DB}`;
 
 app.use(
@@ -20,12 +21,12 @@ app.use(
             readCache: `${__dirname}/postgraphile.cache`,
         }
     )
-  );
+);
 
 app.use(express.static('public'));
 app.post("/post-test", (req, res) => {
     res.send('can post');
-})
+});
 
-const port = process.env.PORT || 3000;
+const port = process.env.GRAPHQL_PORT || 3000;
 app.listen(port, () => console.log(`GraphQL endpoint listening on port ${port}!`))
--- postgraphileOptions.js
+++ postgraphileOptions.js
@@ -1,17 +1,12 @@
-const path = require('path');
-
-const envpath = path.join(__dirname, '/.env');
-require('dotenv').config({path: envpath});
-
 const PgSimplifyInflectorPlugin = require("@graphile-contrib/pg-simplify-inflector");
 const PgFilterPlugin = require("postgraphile-plugin-connection-filter");
 const PgOrderByPlugin = require("@graphile-contrib/pg-order-by-related");
 exports.options = {
   dynamicJson: true,
-  cors: true,
-  graphiql: false,
-  graphqlRoute: '/graphql',
-  externalUrlBase: ``,
+  //cors: true,
+  //graphiql: false,
+  //graphqlRoute: '/graphql',
+  //externalUrlBase: ``,
   pgDefaultRole: process.env.POSTGRES_DEFAULT_ROLE || 'epubtest_public_role',
   // If consuming JWT:
   jwtSecret: process.env.JWT_SECRET,
